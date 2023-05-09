a8e111a69af0bf4e3df761af404181a21ddf3a5d
aranpect <120459638+aranpect@users.noreply.github.com>
2023-05-09 23:34:46 +0000

    first push push

--- src/js/App.tsx
+++ src/js/App.tsx
@@ -1,10 +1,47 @@
-import React, { useCallback, useEffect, useRef, useState } from "react";
-import { MobileView, BrowserView } from "react-device-detect";
+import React, { useState } from 'react';
 
-export default function App() {
+function App() {
+  const [inputValue, setInputValue] = useState('');
+  const [encodedValue, setEncodedValue] = useState('');
+  const [decodedValue, setDecodedValue] = useState('');
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleEncodeClick = () => {
+    const encoded = encodeURIComponent(inputValue+"　君も使おう>>アランペクトで検索<<");
+    navigator.clipboard.writeText("line://"+encoded)
+    setEncodedValue("line://"+encoded);
+  };
+
+  const handleDecodeClick = () => {
+    const decoded = decodeURIComponent(encodedValue);
+    navigator.clipboard.writeText(decoded)
+    setDecodedValue(decoded);
+  };
+
   return (
-    <div className={`relative`}>
-      Hello
+    <div>
+    <header>
+      <h1><a href="https://aranpect.com">Aranpect</a></h1>
+    </header>
+    <div>
+      <div className='width-50 left-color'>
+        <label htmlFor="input">変える文をに入力:</label>
+        <input id="input" type="text" value={inputValue} onChange={handleInputChange} />
+        <button onClick={handleEncodeClick}>変換する</button>
+        {encodedValue && <p>変換したもの: {encodedValue}</p>}
+      </div>
+      <div className='width-50 right-color'>
+        <label htmlFor="encoded">直す文を入力:</label>
+        <input id="encoded" type="text" value={encodedValue} onChange={(e) => setEncodedValue(e.target.value)} />
+        <button onClick={handleDecodeClick}>変換する</button>
+        {decodedValue && <p>変換したもの: {decodedValue}</p>}
+      </div>
+    </div>
     </div>
   );
-};
+}
+
+export default App;

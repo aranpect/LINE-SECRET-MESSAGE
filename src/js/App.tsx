917e8108b870ae72ea1721877b891855a063c610
aranpect <120459638+aranpect@users.noreply.github.com>
2023-05-11 02:31:32 +0000

    sweat alert

--- src/js/App.tsx
+++ src/js/App.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import swal from 'sweetalert'
 
 function App() {
   const [inputValue, setInputValue] = useState('');
@@ -10,15 +11,17 @@ function App() {
   };
 
   const handleEncodeClick = () => {
-    const encoded = encodeURIComponent(inputValue+"　君も使おう>>アランペクトで検索<<");
+    const encoded = encodeURIComponent(inputValue+"　君も使おう>>aranpectで検索<<");
     navigator.clipboard.writeText("line://"+encoded)
-    setEncodedValue("line://"+encoded);
+    swal("変換しコピーしました");
+    setEncodedValue("変換しコピーしました");
   };
 
   const handleDecodeClick = () => {
     const decoded = decodeURIComponent(encodedValue);
     navigator.clipboard.writeText(decoded)
-    setDecodedValue(decoded);
+    Swal("変換しコピーしました");
+    setEncodedValue("変換しコピーしました");
   };
 
   return (
@@ -29,17 +32,22 @@ function App() {
     <div>
       <div className='width-50 left-color'>
         <label htmlFor="input">変える文をに入力:</label>
-        <input id="input" type="text" value={inputValue} onChange={handleInputChange} />
+        <input id="input" type="text" size="40" value={inputValue} onChange={handleInputChange} />
         <button onClick={handleEncodeClick}>変換する</button>
         {encodedValue && <p>変換したもの: {encodedValue}</p>}
       </div>
       <div className='width-50 right-color'>
         <label htmlFor="encoded">直す文を入力:</label>
-        <input id="encoded" type="text" value={encodedValue} onChange={(e) => setEncodedValue(e.target.value)} />
+        <input id="encoded" type="text" size="40" value={encodedValue} onChange={(e) => setEncodedValue(e.target.value)} />
         <button onClick={handleDecodeClick}>変換する</button>
         {decodedValue && <p>変換したもの: {decodedValue}</p>}
       </div>
     </div>
+    <nav>
+      <footer>
+        <p>©Aranpect</p>
+      </footer>
+    </nav>
     </div>
   );
 }

e814e49e1058c492a3e6feb15e44d9af50ac985f
aranpect <120459638+aranpect@users.noreply.github.com>
2023-05-12 00:24:41 +0000

    バグ改善

--- src/js/App.tsx
+++ src/js/App.tsx
@@ -4,6 +4,7 @@ import Swal from 'sweetalert2'
 function App() {
   const [inputValue, setInputValue] = useState('');
   const [encodedValue, setEncodedValue] = useState('');
+  const [encodedValue1, setEncodedValue1] = useState('');
   const [decodedValue, setDecodedValue] = useState('');
 
   const handleInputChange = (e) => {
@@ -18,9 +19,9 @@ function App() {
   };
 
   const handleDecodeClick = () => {
-    const decoded = decodeURIComponent(encodedValue);
+    const decoded = decodeURIComponent(encodedValue1);
     Swal.fire("変換しコピーしました");
-    setEncodedValue("変換しコピーしました");
+    setDecodedValue(decoded);
   };
 
   return (
@@ -33,13 +34,13 @@ function App() {
         <label htmlFor="input">変える文をに入力:</label>
         <input id="input" type="text" value={inputValue} onChange={handleInputChange} />
         <button onClick={handleEncodeClick}>変換する</button>
-        {encodedValue && <p>変換したもの: {encodedValue}</p>}
+        {encodedValue && <p>{encodedValue}</p>}
       </div>
       <div className='width-50 right-color'>
         <label htmlFor="encoded">直す文を入力:</label>
-        <input id="encoded" type="text" value={encodedValue} onChange={(e) => setEncodedValue(e.target.value)} />
+        <input id="encoded" type="text" value={encodedValue1} onChange={(e) => setEncodedValue1(e.target.value)} />
         <button onClick={handleDecodeClick}>変換する</button>
-        {decodedValue && <p>変換したもの: {decodedValue}</p>}
+        {decodedValue && <p>{decodedValue}</p>}
       </div>
     </div>
     </div>
